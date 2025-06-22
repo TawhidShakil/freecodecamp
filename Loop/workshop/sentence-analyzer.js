5a9560cdcab8f0b8060319a8f9ade27e9c6c079f
TawhidShakil <astalukdar39@gmail.com>
2025-06-22 22:21:16 +0600

    add word count function

--- Loop/workshop/sentence-analyzer.js
+++ Loop/workshop/sentence-analyzer.js
@@ -45,3 +45,69 @@ function getPunctuationCount(sentence){
 
 const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
 console.log(`Punctuation Count: ${punctuationCount}`);
+
+
+// word count function
+
+function getVowelCount(sentence) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+console.log(`Vowel Count: ${vowelCount}`);
+
+function getConsonantCount(sentence) {
+  const consonants = "bcdfghjklmnpqrstvwxyz";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (consonants.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const consonantCount = getConsonantCount("Coding is fun");
+console.log(`Consonant Count: ${consonantCount}`);
+
+function getPunctuationCount(sentence) {
+  const punctuations = ".,!?;:-()[]{}\"'–";
+  let count = 0;
+
+  for (const char of sentence) {
+    if (punctuations.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+function getWordCount(sentence){
+  let numberOfWord = 0;
+  let isWord = false;
+  for(let i=0; i<sentence.length; i++){
+    const char = sentence[i];
+    if(char !== " " && !isWord){
+      isWord = true;
+      numberOfWord++;
+    }else if(char === " "){
+      isWord = false;
+    }
+  }
+  return numberOfWord;
+}
+
+const wordCount = getWordCount("I love freeCodeCamp")
+console.log(`Word Count: ${wordCount}`);
